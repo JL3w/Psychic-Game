2c39bc46b86ee1e7ab101ef9a814f13d8d3470ae
J Lew <momentary232@gmail.com>
2019-02-25 15:46:09 -0500

    html / javascript added, basic functioning game

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Psychic-Game
 Psychic Guessing Game
+This is my very first project. It demonstrates my basic understanding of html, bootstrap, and javascript. 
---
+++ assets/javascript/game.js
@@ -0,0 +1,51 @@
+
+var computerOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+console.log(computerChoice)
+
+
+var psychic = {
+    wins: 0,
+    losses: 0,
+    guessesLeft: 10,
+    guessesSoFar: [],
+
+    gameReset: function() {
+        this.guessesLeft = 10;
+        this.guessesSoFar = [];
+        computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
+    }
+}
+
+
+
+document.onkeyup = function(event) {
+    var guessedLetter = event.key.toLowerCase();
+    console.log(guessedLetter);
+    console.log(computerChoice);
+    if (guessedLetter === computerChoice) {
+        psychic.wins++;
+        alert("Congrats you're an Indigo, you selected " + guessedLetter + " and you picked correctly with " + [psychic.guessesLeft] + " guesses left!" );
+        psychic.gameReset();
+
+    }
+
+    else {
+        psychic.guessesLeft--;
+        psychic.guessesSoFar.push(guessedLetter)
+        if (psychic.guessesLeft < 1) {
+            psychic.losses++;
+            alert("You done goofed, you guessed " + [psychic.guessesSoFar] + " and you still couldn't get it, total Normie.");
+            psychic.gameReset();
+        }
+    }
+
+    var html =
+          "<H2>Wins: " + psychic.wins + "</h2>" +
+          "<h2>Losses: " + psychic.losses + "</h2>" +
+          "<h2>Guesses Left: " + psychic.guessesLeft + "</h2>" +
+          "<h2>Guesses so far: " + psychic.guessesSoFar + "<h2>";
+
+        document.querySelector("#game-content").innerHTML = html;
+}
+
